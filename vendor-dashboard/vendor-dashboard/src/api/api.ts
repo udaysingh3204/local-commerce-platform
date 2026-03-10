6102f885698b6239f102c5dc9eb3da15daf34a0a
unknown <you@example.com>
2026-03-10 13:01:02 +0530

    API base URL change

--- vendor-dashboard/vendor-dashboard/src/api/api.ts
+++ vendor-dashboard/vendor-dashboard/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://local-commerce-platform-production.up.railway.app/api"
 });
 
 export default API;
